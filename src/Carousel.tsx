ca1f027b887253a4f373a144667dadce66fd260f
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-06-19 02:51:10 +0300

    refactor: :label: Typing Carousel

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,6 +1,10 @@
-import { Component } from "react";
+import { Component, MouseEvent } from "react";
 
-class Carousel extends Component {
+interface IProps {
+  images: string[];
+}
+
+class Carousel extends Component<IProps> {
   state = {
     active: 0,
   };
@@ -9,7 +13,11 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
-  handleIndexClick = (event) => {
+  handleIndexClick = (event :MouseEvent<HTMLElement>) => {
+    if (!(event.target instanceof HTMLElement) || !event.target.dataset.index) {
+      return;
+    }
+    
     this.setState({
       active: +event.target.dataset.index,
     });
