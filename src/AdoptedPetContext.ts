6948770c6e8a1309fa2c0c772cc6fddaf4b412a5
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-06-19 05:29:02 +0300

    refactor: :label: Typing the rest & add scripts

--- src/AdoptedPetContext.ts
+++ src/AdoptedPetContext.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import { Pet } from "./APIResponseTypes";
 
 // I don't know why 'Eng. Holt' doesn't use the as operator here, but I'm going to use it.
-const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+const AdoptedPetContext = createContext<[Pet | null, (adoptedPet: Pet) => void]>([
   {} as Pet,
   () => {},
 ]);
